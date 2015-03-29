50f43bb40e7d3463bdecf9efd55e78dcdb23777c
Kevin Sjöberg <kev.sjoberg@gmail.com>
2015-03-29 16:56:05 +0200

    Use sprites instead of rects

--- static/src/js/bomberman.js
+++ static/src/js/bomberman.js
@@ -1,5 +1,73 @@
 (function () {
-  var boardSize = 500;
+  var newImage = function (path) {
+    return _.tap(new Image(), function (img) {
+      img.src = path;
+    });
+  };
+  var loadSprites = function (requestedSprites) {
+    var sprites = {}
+    _.each(requestedSprites, function (path, key) {
+      if (_.isArray(path)) {
+        sprites[key] = _.map(path, newImage);
+      } else {
+        sprites[key] = newImage(path);
+      }
+    });
+    return sprites;
+  };
+  var determineSprite = function (player) {
+    if (!player.movement) {
+      return playerSprites["down"][0];
+    }
+    var tileSpeed = (boardSize / tileRoot) / (boardSize * player.movement.speed) * 1000;
+    var milliseconds = new Date().getMilliseconds();
+    var spriteIndex = Math.floor((milliseconds / (tileSpeed / 8))) % 8;
+    return playerSprites[player.movement.direction][spriteIndex];
+  };
+  var blockSprites = loadSprites({
+    g: "images/Blocks/BackgroundTile.png",
+    w: "images/Blocks/ExplodableBlock.png",
+    s: "images/Blocks/SolidBlock.png",
+  });
+  var playerSprites = loadSprites({
+    up: [
+      "images/Bomberman/Up/f00.png",
+      "images/Bomberman/Up/f01.png",
+      "images/Bomberman/Up/f02.png",
+      "images/Bomberman/Up/f03.png",
+      "images/Bomberman/Up/f04.png",
+      "images/Bomberman/Up/f05.png",
+      "images/Bomberman/Up/f06.png",
+      "images/Bomberman/Up/f07.png"],
+    right: [
+      "images/Bomberman/Right/f00.png",
+      "images/Bomberman/Right/f01.png",
+      "images/Bomberman/Right/f02.png",
+      "images/Bomberman/Right/f03.png",
+      "images/Bomberman/Right/f04.png",
+      "images/Bomberman/Right/f05.png",
+      "images/Bomberman/Right/f06.png",
+      "images/Bomberman/Right/f07.png"],
+    down: [
+      "images/Bomberman/Down/f00.png",
+      "images/Bomberman/Down/f01.png",
+      "images/Bomberman/Down/f02.png",
+      "images/Bomberman/Down/f03.png",
+      "images/Bomberman/Down/f04.png",
+      "images/Bomberman/Down/f05.png",
+      "images/Bomberman/Down/f06.png",
+      "images/Bomberman/Down/f07.png"],
+    left: [
+      "images/Bomberman/Left/f00.png",
+      "images/Bomberman/Left/f01.png",
+      "images/Bomberman/Left/f02.png",
+      "images/Bomberman/Left/f03.png",
+      "images/Bomberman/Left/f04.png",
+      "images/Bomberman/Left/f05.png",
+      "images/Bomberman/Left/f06.png",
+      "images/Bomberman/Left/f07.png"],
+  });
+  var boardSize = 640;
   var tileRoot = 10;
   var tileSize = boardSize / tileRoot;
   var playerSize = boardSize * 0.06;
@@ -9,14 +77,14 @@
   var ctx = canvas.getContext("2d");
   var renderBoard = function (board) {
     _.each(board, function (tile, i) {
-      ctx.fillStyle = {g: "rgb(1, 166, 17)", s: "rgb(139, 141, 122)", w: "rgb(102, 51, 0)"}[tile];
-      ctx.fillRect((i % tileRoot) * tileSize, Math.floor(i / tileRoot) * tileSize, tileSize, tileSize);
+      ctx.drawImage(blockSprites[tile], (i % tileRoot) * tileSize, Math.floor(i / tileRoot) * tileSize, tileSize, tileSize);
     });
   };
   var renderPlayers = function (players) {
     _.each(players, function (player) {
-      ctx.fillStyle = "rgb(255, 255, 255)";
-      ctx.fillRect(Math.floor(player.dimension.x * boardSize) - (playerSize / 2), Math.floor(player.dimension.y * boardSize) - (playerSize / 2), playerSize, playerSize);
+      var sprite = determineSprite(player);
+      var playerHeight = sprite.height * (playerSize / sprite.width);
+      ctx.drawImage(sprite, Math.floor(player.dimension.x * boardSize) - (playerSize / 2), Math.floor(player.dimension.y * boardSize) - (playerHeight / 2), playerSize, playerHeight);
     });
   };
   var render = function (game) {
